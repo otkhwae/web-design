5b736232cee2604d2a9ab68d8ca3b4662be7ee34
Ot Khwae <otkhwae@gmail.com>
2023-08-18 13:26:15 +0200

    props with functional component

---
+++ react/hello-world/src/components/greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function greet () {
+    return <h1>hello ot</h1>
+}
+
+export default greet
+
+
+
+
+
+
+
---
+++ react/my-app/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import {Component} from 'react'
+
+class Welcome extends Component {
+    render() {
+        return <h1>hi from welcome class component</h1>
+    }
+} 
+
+export default Welcome
